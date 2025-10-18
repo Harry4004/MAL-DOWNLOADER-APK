e500d1666a796f8bb434699a14c8f46c3fee8d16
Harry4004 <harshitkhatriofficial@gmail.com>
2025-10-18 18:32:45 +0530

    Update MainApplication.kt

--- app/src/main/java/com/harry/maldownloader/MainApplication.kt
+++ app/src/main/java/com/harry/maldownloader/MainApplication.kt
@@ -4,19 +4,21 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
-import androidx.core.app.NotificationManagerCompat
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
+    }
 
+    private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
-                getString(R.string.notification_channel_id),
-                getString(R.string.notification_channel_name),
+                "download_channel",
+                "Download Progress",
                 NotificationManager.IMPORTANCE_LOW
             ).apply {
-                description = getString(R.string.notification_channel_desc)
+                description = "Shows download progress notifications"
             }
             val manager = getSystemService(NotificationManager::class.java)
             manager.createNotificationChannel(channel)
