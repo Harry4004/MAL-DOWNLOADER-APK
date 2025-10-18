22e4cb93592f324d58adfb22c47473a06a7a97bb
Harry4004 <harshitkhatriofficial@gmail.com>
2025-10-18 09:31:53 +0530

    Add Material 3 color system and typography definitions

---
+++ app/src/main/java/com/harry/maldownloader/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.harry.maldownloader.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// MAL-specific colors
+val MALBlue = Color(0xFF2E51A2)
+val MALLightBlue = Color(0xFF4FC3F7)
+val AnimeGreen = Color(0xFF4CAF50)
+val MangaOrange = Color(0xFFFF9800)
+val HentaiPink = Color(0xFFE91E63)
