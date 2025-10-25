a6e1032b6d6744fcaf67de4182df42d520de2ae9
Harry4004 <harshitkhatriofficial@gmail.com>
2025-10-25 14:34:05 +0530

    Fix Kotlin syntax error in TagManagerDialog.kt (incorrect import keyword)

--- app/src/main/java/com/harry/maldownloader/ui/components/TagManagerDialog.kt
+++ app/src/main/java/com/harry/maldownloader/ui/components/TagManagerDialog.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-имport com.harry.maldownloader.MainViewModel
+import com.harry.maldownloader.MainViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
